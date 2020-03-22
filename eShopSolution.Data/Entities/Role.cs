ff3c0b477754bc8c710fa65c672e4c342676a59e
thevinh97 <thevinh.nh.kh.1997@gmail.com>
2020-03-22 17:09:23 +0700

    design DB

---
+++ eShopSolution.Data/Entities/Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Entities
+{
+    public class Role
+    {
+        public int Id { get; set; } 
+        public int Name { get; set; } 
+        public int Description { get; set; } 
+
+    }
+}
